82152aa21ff051e6d77ac8e42a8b6049b469ab28
Z0ro0 <chlrkdmf0805@gmail.com>
2023-04-16 00:40:40 +0900

    지금까지 한 거 react로 변경

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container1">
+        <div id="user">
+            <img id="profile" src="img/user.png"/>
+    
+            <div id="userinfo">
+                <p class="username">yang hyewon</p>
+                <p class="useremail">w2106@e-mirim.hs.kr</p>
+            </div>
+        </div>
+        
+        <div id="usertable">
+            <div class="grid-container">
+                <div class="grid-item days-of-the-week">월</div>
+                <div class="grid-item days-of-the-week">화</div>
+                <div class="grid-item days-of-the-week">수</div>
+                <div class="grid-item days-of-the-week">목</div>
+                <div class="grid-item days-of-the-week">금</div>
+      
+    
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+                <div class="grid-item1">국어</div>
+            </div>
+        </div>
+    </div>
+    
+<div id="container2">
+    <div id="usertime">
+        <p class="time1">2 : 00 : 00</p>
+        <p class="time2">2 : 00 : 00</p>
+        <p class="time3">2 : 00 : 00</p>
+        <p class="time4">2 : 00 : 00</p>
+    </div>
+
+    <div id="usercalendar">
+        <div class="grid-container2">
+            <div class="grid-item days-of-the-week">월</div>
+            <div class="grid-item days-of-the-week">화</div>
+            <div class="grid-item days-of-the-week">수</div>
+            <div class="grid-item days-of-the-week">목</div>
+            <div class="grid-item days-of-the-week">금</div>
+            <div class="grid-item days-of-the-week">토</div>
+            <div class="grid-item days-of-the-week">일</div>
+    
+            <div class="grid-item">1</div>
+            <div class="grid-item">2</div>
+            <div class="grid-item">3</div>
+            <div class="grid-item">4</div>
+            <div class="grid-item">5</div>
+            <div class="grid-item">6</div>
+            <div class="grid-item">7</div>
+            <div class="grid-item">8</div>
+            <div class="grid-item">9</div>
+            <div class="grid-item">10</div>
+            <div class="grid-item">11</div>
+            <div class="grid-item">12</div>
+            <div class="grid-item">13</div>
+            <div class="grid-item">14</div>
+            <div class="grid-item">15</div>
+            <div class="grid-item">16</div>
+            <div class="grid-item">17</div>
+            <div class="grid-item">18</div>
+            <div class="grid-item">19</div>
+            <div class="grid-item">20</div>
+            <div class="grid-item">21</div>
+            <div class="grid-item">22</div>
+            <div class="grid-item">23</div>
+            <div class="grid-item">24</div>
+            <div class="grid-item">25</div>
+            <div class="grid-item">26</div>
+            <div class="grid-item">27</div>
+            <div class="grid-item">28</div>
+            <div class="grid-item">29</div>
+            <div class="grid-item">30</div>
+        </div>
+    </div>
+</div>
     </div>
   );
 }
