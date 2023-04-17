d53e93d44c4bac15e5fe256e8824b31619e536fd
Z0ro0 <chlrkdmf0805@gmail.com>
2023-04-18 00:31:55 +0900

    폰트 변경, grid

--- src/App.js
+++ src/App.js
@@ -59,56 +59,75 @@ function App() {
             </div>
             
         <div id="container2">
-            <div id="usertime">
-                <p class="time1">2 : 00 : 00</p>
-                <p class="time2">2 : 00 : 00</p>
-                <p class="time3">2 : 00 : 00</p>
-                <p class="time4">2 : 00 : 00</p>
-            </div>
-
-            <div id="usercalendar">
-                <div class="grid-container2">
-                    <div class="grid-item days-of-the-week">월</div>
-                    <div class="grid-item days-of-the-week">화</div>
-                    <div class="grid-item days-of-the-week">수</div>
-                    <div class="grid-item days-of-the-week">목</div>
-                    <div class="grid-item days-of-the-week">금</div>
-                    <div class="grid-item days-of-the-week">토</div>
-                    <div class="grid-item days-of-the-week">일</div>
-            
-                    <div class="grid-item">1</div>
-                    <div class="grid-item">2</div>
-                    <div class="grid-item">3</div>
-                    <div class="grid-item">4</div>
-                    <div class="grid-item">5</div>
-                    <div class="grid-item">6</div>
-                    <div class="grid-item">7</div>
-                    <div class="grid-item">8</div>
-                    <div class="grid-item">9</div>
-                    <div class="grid-item">10</div>
-                    <div class="grid-item">11</div>
-                    <div class="grid-item">12</div>
-                    <div class="grid-item">13</div>
-                    <div class="grid-item">14</div>
-                    <div class="grid-item">15</div>
-                    <div class="grid-item">16</div>
-                    <div class="grid-item">17</div>
-                    <div class="grid-item">18</div>
-                    <div class="grid-item">19</div>
-                    <div class="grid-item">20</div>
-                    <div class="grid-item">21</div>
-                    <div class="grid-item">22</div>
-                    <div class="grid-item">23</div>
-                    <div class="grid-item">24</div>
-                    <div class="grid-item">25</div>
-                    <div class="grid-item">26</div>
-                    <div class="grid-item">27</div>
-                    <div class="grid-item">28</div>
-                    <div class="grid-item">29</div>
-                    <div class="grid-item">30</div>
+            <div id="usertc">
+                <div id="usertimecal">
+                    <p class="cal1">3/22</p>
+                    <p class="cal2">3/22</p>
+                    <p class="cal3">3/22</p>
+                    <p class="cal4">3/22</p>
+                    <p class="cal4">3/22</p>
+                    <p class="cal4">3/22</p>
+                    <p class="cal4">3/22</p>
+                    <p class="cal4">3/22</p>
+                    <p class="cal4">3/22</p>
+                </div>
+                    
+                <div id="usertime">
+                    <p class="time1">2 : 00 : 00</p>
+                    <p class="time2">2 : 00 : 00</p>
+                    <p class="time3">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
+                    <p class="time4">2 : 00 : 00</p>
                 </div>
             </div>
-        </div>
+
+                <div id="usercalendar">
+                    <div class="grid-container2">
+                        <div class="grid-item days-of-the-week">월</div>
+                        <div class="grid-item days-of-the-week">화</div>
+                        <div class="grid-item days-of-the-week">수</div>
+                        <div class="grid-item days-of-the-week">목</div>
+                        <div class="grid-item days-of-the-week">금</div>
+                        <div class="grid-item days-of-the-week">토</div>
+                        <div class="grid-item days-of-the-week">일</div>
+                
+                        <div class="grid-item">1</div>
+                        <div class="grid-item">2</div>
+                        <div class="grid-item">3</div>
+                        <div class="grid-item">4</div>
+                        <div class="grid-item">5</div>
+                        <div class="grid-item">6</div>
+                        <div class="grid-item">7</div>
+                        <div class="grid-item">8</div>
+                        <div class="grid-item">9</div>
+                        <div class="grid-item">10</div>
+                        <div class="grid-item">11</div>
+                        <div class="grid-item">12</div>
+                        <div class="grid-item">13</div>
+                        <div class="grid-item">14</div>
+                        <div class="grid-item">15</div>
+                        <div class="grid-item">16</div>
+                        <div class="grid-item">17</div>
+                        <div class="grid-item">18</div>
+                        <div class="grid-item">19</div>
+                        <div class="grid-item">20</div>
+                        <div class="grid-item">21</div>
+                        <div class="grid-item">22</div>
+                        <div class="grid-item">23</div>
+                        <div class="grid-item">24</div>
+                        <div class="grid-item">25</div>
+                        <div class="grid-item">26</div>
+                        <div class="grid-item">27</div>
+                        <div class="grid-item">28</div>
+                        <div class="grid-item">29</div>
+                        <div class="grid-item">30</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   );
