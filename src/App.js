cd3314105a44bbd5604ace3682ff7af7923ebcbf
hyewon <w2106@e-mirim.hs.kr>
2023-05-31 18:05:31 +0900

    studyTime

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import './Navbar.css';
 import logo from './logo.png';
@@ -6,99 +6,82 @@ import leftIcon from './free-icon-font-angle-left-3916934.svg';
 import rightIcon from './free-icon-font-angle-right-3916924.svg';
 
 function App() {
-    useEffect(() => {
-        // 현재 구하자
-        let now = new Date();
-        // 현재 년
-        let year = now.getFullYear();
-        // 현재 월
-        let month = now.getMonth(); //0~11
-        month++;
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
 
+  //calendar, studyTime 월 변경
+  useEffect(() => {
+    const setCalendar = (year, month) => {
+      let firstDate = new Date(year, month - 1, 1);
+      let firstDay = firstDate.getDay();
+      let lastDate = new Date(year, month, 0).getDate();
 
-        const setCalendar = (year, month) => {
-            // 1일이 무슨 요일?
-            let firstDate = new Date(year, month - 1, 1);
-            let firstDay = firstDate.getDay();
+      const setTitle = (year, month) => {
+        let title_year = document.getElementById("title_year");
+        title_year.innerHTML = year;
+        let title_month = document.getElementById("title_month");
+        title_month.innerHTML = month;
+      };
+      setTitle(year, month);
 
-            // 말일은 며칠?
-            let lastDate = new Date(year, month, 0).getDate();  //2022년 10월 0일 = 2022년 9월 말일
+      const dateGridContainerDiv = document.getElementsByClassName("date-grid-container")[0];
+      dateGridContainerDiv.innerHTML = "";
 
-            // 제목 표시하자
-            const setTitle = (year, month) => {
-                // console.log(`${year}년 ${month}월`)
-                let title_year = document.getElementById("title_year");
-                title_year.innerHTML = year;    //니꼴라스 says
-                let title_month = document.getElementById("title_month");
-                title_month.innerHTML = month
-            }
-            setTitle(year, month);
+      for (let i = 1; i <= lastDate; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("grid-item");
+        newDiv.innerHTML = i;
+        dateGridContainerDiv.appendChild(newDiv);
+      }
 
-            const dateGridContainerDiv = document.getElementsByClassName("date-grid-container")[0];
-            dateGridContainerDiv.innerHTML = "";    //초기화
-            // 1~말일까지 grid-item 만들자
-            for (let i = 1; i <= lastDate; i++) {
-                //요소 만들자
-                let newDiv = document.createElement("div");
-                //class에 grid-item 넣자
-                newDiv.classList.add("grid-item");
-                //text에 숫자 넣자
-                newDiv.innerHTML = i;
-                //부모에 newDiv 달자
-                dateGridContainerDiv.appendChild(newDiv);
-            }
+      let firstDateDiv = dateGridContainerDiv.getElementsByClassName("grid-item")[0];
+      firstDateDiv.style.gridColumnStart = firstDay + 1;
+    };
 
-            // 1일에 해당하는 div를 grid-column-start: 요일 + 1;
-            let firstDateDiv = dateGridContainerDiv.getElementsByClassName("grid-item")[0];
-            firstDateDiv.style.gridColumnStart = firstDay + 1;
+    setCalendar(year, month);
+  }, [year, month]);
 
-        }
-        setCalendar(year, month);
-        //이전 달 달력 보이자
-        const prevMonth = () => {
-            month--;
-            //month가 0이면, month = 12, year--;
-            if (month == 0) {
-                year--;
-                month = 12;
-            }
-            setCalendar(year, month);
-        }
+  const prevMonth = () => {
+    let newMonth = month - 1;
+    let newYear = year;
+    if (newMonth === 0) {
+      newYear--;
+      newMonth = 12;
+    }
+    setYear(newYear);
+    setMonth(newMonth);
+  };
 
-        //다음 달 달력 보이자
-        const nextMonth = () => {
-            month++;
-            //month가 13이면, month = 1, year++;
-            if (month == 13) {
-                year++;
-                month = 1;
-            }
-            setCalendar(year, month);
-        }
+  const nextMonth = () => {
+    let newMonth = month + 1;
+    let newYear = year;
+    if (newMonth === 13) {
+      newYear++;
+      newMonth = 1;
+    }
+    setYear(newYear);
+    setMonth(newMonth);
+  };
 
-        const initButton = () => {
-            const prev_btn = document.getElementById("prev_btn");
-            const next_btn = document.getElementById("next_btn");
+  //최근 studyTime
+  const [studyTimeData] = useState([
+    { date: '5/25', time: '02:00:00' },
+    { date: '5/26', time: '02:00:00' },
+    { date: '5/27', time: '02:00:00' },
+    { date: '5/28', time: '02:00:00' },
+    { date: '5/29', time: '02:00:20' },
+    { date: '5/30', time: '02:00:00' },
+    { date: '5/31', time: '02:00:00' },
+  ]);
 
-            //js event 달자
-            // prev_btn.addEventListener("click", prevMonth);
-            // next_btn.addEventListener("click", nextMonth);
-            prev_btn.onclick = prevMonth;
-            next_btn.onclick = nextMonth;
-        }
-        initButton();
-
-    
-    }, []);
-
-    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-    const timeSlots = ['1', '2', '3', '4', '5', '6', '7'];
-
-    const timetableData = [
-        { day: 'Mon', time: '1', subject: '국어' },
-        { day: 'Tue', time: '2', subject: 'DB' },
-        { day: 'Thu', time: '3', subject: 'UI/UX' },
-    ];
+  const renderStudyTimeRows = () => {
+    return studyTimeData.map((item, index) => (
+      <tr key={index}>
+        <td className='studyDate'>{item.date}</td>
+        <td className='studyTime'>{item.time}</td>
+      </tr>
+    ));
+  };
 
   return (
     <div className="App">
@@ -109,10 +92,10 @@ function App() {
                 </a>
                 <ul className='nav-list'>
                     <li className='community'>
-                        <a href="community.html">Community</a>
+                    <a href="community.html">Community</a>
                     </li>
                     <li className='mypage'>
-                        <a href="myPage.html">My Page</a>
+                    <a href="myPage.html">My Page</a>
                     </li>
                 </ul>
             </div>
@@ -120,87 +103,59 @@ function App() {
         <div id="container">
             <div id="container1">
                 <div id="user">
-                    <img id="profile" src="./img/user1.png" alt="profile"/>
-            
+                    <img id="profile" src="./img/user1.png" alt="profile" />
+
                     <div id="userinfo">
                         <p className="username">yang hyewon</p>
                         <p className="useremail">w2106@e-mirim.hs.kr</p>
+                        <button className='userEdit'>EDIT</button>
                     </div>
                 </div>
-                
-                <div id="usertable">
-                <table className='timetable'>
-                <thead>
-                    <tr>
-                    <th></th>
-                    {daysOfWeek.map(day => (
-                        <th className='th' key={day}>{day}</th>
-                    ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {timeSlots.map(time => (
-                    <tr key={time}>
-                        <td className='td td-time'>{time}</td>
-                        {daysOfWeek.map(day => (
-                        <td key={day + time} className='td td-data'>
-                            {/* 해당 셀에 항목이 있는 경우 표시합니다. */}
-                            {timetableData.find(item => item.day === day && item.time === time)?.subject}
-                        </td>
-                        ))}
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
+
+             <div className='totalTime'>
+                    <h1 className='totalH1'>{year}년 {month}월 총 공부시간</h1>
                 </div>
             </div>
-            
+
             <div id="container2">
-                <div id="usertc">
-                    <div id="usertimecal">
-                        <p className="cal1">3/22</p>
-                        <p className="cal2">3/22</p>
-                        <p className="cal3">3/22</p>
-                        <p className="cal4">3/22</p>
-                        <p className="cal4">3/22</p>
-                        <p className="cal4">3/22</p>
-                        <p className="cal4">3/22</p>
-                        <p className="cal4">3/22</p>
-                        <p className="cal4">3/22</p>
-                    </div>
-                        
-                    <div id="usertime">
-                        <p className="time1">2 : 00 : 00</p>
-                        <p className="time2">2 : 00 : 00</p>
-                        <p className="time3">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                        <p className="time4">2 : 00 : 00</p>
-                    </div>
+                <div id="usertimecal">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th className='studyTimeTh'>Date</th>
+                            <th className='studyTimeTh'>Time</th>
+                        </tr>
+                        </thead>
+                        <tbody>{renderStudyTimeRows()}</tbody>
+                    </table>
                 </div>
 
                 <div id="usercalendar">
                     <div className="calendar">
                         <div className="flex-container">
-                            <div id="prev_btn"><img src={leftIcon} className='angle-icon'></img></div>
-                                <h1 className='calendar-h1'><span id="title_year"></span>년 <span id="title_month"></span>월</h1>
-                            <div id="next_btn"><img src={rightIcon} className='angle-icon'></img></div>
+                            <div id="prev_btn" onClick={prevMonth}>
+                                <img src={leftIcon} className='angle-icon' alt="Prev" />
+                            </div>
+                            <h1 className='calendar-h1'>
+                            <span id="title_year"></span>년 <span id="title_month"></span>월
+                            </h1>
+                            <div id="next_btn" onClick={nextMonth}>
+                                <img src={rightIcon} className='angle-icon' alt="Next" />
+                            </div>
                         </div>
                         <div id="calendar">
                             <div className="grid-container-calendar">
-                                <div className="grid-item days-of-the-week-calendar">일</div>
-                                <div className="grid-item days-of-the-week-calendar">월</div>
-                                <div className="grid-item days-of-the-week-calendar">화</div>
-                                <div className="grid-item days-of-the-week-calendar">수</div>
-                                <div className="grid-item days-of-the-week-calendar">목</div>
-                                <div className="grid-item days-of-the-week-calendar">금</div>
-                                <div className="grid-item days-of-the-week-calendar">토</div>
+                            <div className="grid-item days-of-the-week-calendar">일</div>
+                            <div className="
+                            
+                            -item days-of-the-week-calendar">월</div>
+                            <div className="grid-item days-of-the-week-calendar">화</div>
+                            <div className="grid-item days-of-the-week-calendar">수</div>
+                            <div className="grid-item days-of-the-week-calendar">목</div>
+                            <div className="grid-item days-of-the-week-calendar">금</div>
+                            <div className="grid-item days-of-the-week-calendar">토</div>
                             </div>
-                            <div className="date-grid-container">
-                            </div>
+                            <div className="date-grid-container"></div>
                         </div>
                     </div>
                 </div>
